b4bcbed6f1b8b095e5581e332fa6afcf4275fb9f
Heather Bjoin <h.m.bjoin@gmail.com>
2021-10-16 18:32:02 -0500

    Making contact form functional

--- react-portfolio/src/components/Form.js
+++ react-portfolio/src/components/Form.js
@@ -1,145 +1,4 @@
-// import React, {useState, useRef, useEffect} from 'react';
-
-// import '../styles/Contact.css';
-
-// import { Button, TextField, Collapse  } from '@material-ui/core';
-
-// import { validateEmail } from '../utils/helpers';
-
-
-
-
-//     function Form() {
-//     const [firstName, setFirstName] = useState('');
-//     const [lastName, setLastName] = useState('');
-//     const [email, setEmail] = useState('')
-//     const [message, setMessage] = useState('')
-//     const [errorMessage, setErorrMessage] = useState('');
-    
-
-//     const handleInputChange = (e) => {
-//         let target = e.target;
-//         let inputType = target.name;
-//         let inputValue = target.value;
-
-
-//         switch(inputType) {
-//             case 'email':
-//                 return setEmail(inputValue);
-//             case 'firstName':
-//                 return setFirstName(inputValue);
-//             case 'lastName':
-//                 return setLastName(inputValue);
-//             case 'message':
-//                 return setMessage(inputValue);
-//         }
-
-//         if (inputType === 'email') {
-//             setEmail(inputValue);
-//             } else if (inputType === 'firstName') {
-//             setFirstName(inputValue);
-//             } else if (inputType === 'lastName') {
-//             setLastName(inputValue);
-//             } else if (inputType === 'message') {
-//             setMessage(inputValue);
-//             }
-//         return;
-//     };
-
-//     const handleFormSubmit = (e) => {
-//         e.preventDefault();
-//         if (!validateEmail(email)) {
-//             setErorrMessage(`Please enter a valid email`);
-//         } else if (!firstName) {
-//             setErorrMessage(`First name is required`);
-//         } else if (!lastName) {
-//             setErorrMessage(`Last name is required`);
-//         } else if (!message) {
-//             setErorrMessage(`A message is required!`);
-//         } 
-//         ;
-
-//         setFirstName('');
-//         setLastName('');
-//         setEmail('');
-//         setMessage('');
-//     };
-
-
-
-//     return (
-
-//         <div>
-//             <form className="form" style={{
-//                 display: 'flex',
-//                 flexDirection: 'column',
-//                 alignItems: 'center',
-
-//             }}>
-//                 <input
-//                     className="contact-input"
-//                     value={firstName}
-//                     name="firstName"
-//                     required onChange={this.handleChange}
-//                     type="text"
-//                     placeholder="First Name"
-//                     required
-//                 />
-//                 {errorMessage && (
-//                 <div>
-//                     <p className="error-text">{errorMessage}</p>
-//                 </div>
-//                 )}
-
-//                 <input
-//                     className="contact-input"
-//                     value={lastName}
-//                     name="lakeName"
-//                     onChange={handleInputChange} required
-//                     type="text"
-//                     placeholder="Last Name"
-//                 />
-//                 <input
-//                     className="contact-input"
-//                     value={email}
-//                     name="email"
-//                     onChange={handleInputChange}
-//                     type="email"
-//                     placeholder="email"
-//                 />
-//                 <textarea
-//                     className="contact-input content-textarea"
-//                     value={message}
-//                     name="message"
-//                     onChange={handleInputChange}
-//                     type="text"
-//                     placeholder="Please enter your message here."
-//                 />
-//                 <button class="contact-btn" type="button" onClick={handleFormSubmit}>
-//                     Submit
-//                 </button>
-//             </form>
-//             {errorMessage && (
-//                 <div>
-//                     <p className="error-text">{errorMessage}</p>
-//                 </div>
-//             )}
-//         </div>
-//     )
-// };
-// export default Form;
-
-
-
-
-
-
-
-
-
-
-
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { Typography, Button, Container, IconButton, Link} from '@material-ui/core'
 
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
@@ -148,6 +7,7 @@ import TextField from '@material-ui/core/TextField'
 import { validateEmail } from '../utils/helpers';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import SvgIcon from '@material-ui/core/SvgIcon';
+import emailjs from 'emailjs-com';
 
 const useStyles = makeStyles({
   field: {
@@ -233,6 +93,43 @@ export default function Create() {
 
   }
 
+  const formRef = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_r9ncuc1', 'template_julz5pc', formRef.current, 'user_SSCp2Ltnckp7WJ06JAVTW')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      
+      setNameError(false);
+      setEmailError(false);
+      setMessageError(false);
+  
+      if (name === ''){
+        setNameError(true)
+      };
+      if (email === '' || !validateEmail(email)){
+        setEmailError(true)
+      };
+      if (message === ''){
+        setMessageError(true)
+      };
+  
+  
+      if (name && email && message) {
+        console.log(name, email, message)
+      }
+  
+      setName('');
+      setEmail('');
+      setMessage('');
+  };
+
+
   return (
     <Container>
       <Typography
@@ -242,10 +139,11 @@ export default function Create() {
       gutterBottom>
       </Typography>
 
-      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+      <form noValidate autoComplete="off" onSubmit={sendEmail} ref={formRef}>
         <TextField
           onChange={(e) => setName(e.target.value)}
           className={classes.field}
+          name="name"
           label="Name"
           value={name}
           variant="outlined"
@@ -261,6 +159,7 @@ export default function Create() {
           onChange={(e) => setEmail(e.target.value)}
           className={classes.field}
           label="Email"
+          name="email"
           value={email}
           variant="outlined"
           color="primary"
@@ -275,6 +174,7 @@ export default function Create() {
           onChange={(e) => setMessage(e.target.value)}
           className={classes.field}
           label="Message"
+          name="message"
           variant="outlined"
           color="primary"
           value={message}
@@ -288,7 +188,7 @@ export default function Create() {
         />
 
         <Button 
-        onClick={handleSubmit}
+        onClick={sendEmail}
         type="submit"
         color="secondary"
         variant="contained"
