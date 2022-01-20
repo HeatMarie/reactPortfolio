f1fe7b3d520652505d98861b2a201da57a0bec21
Heather Bjoin <h.m.bjoin@gmail.com>
2022-01-19 23:08:02 -0600

    Making changes to footer. Size, color, letting spacing, etc

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -11,42 +11,31 @@ import '../styles/Footer.css';
 
 const Footer = () => {
 
-    const theme = createTheme({
-        palette: {
-            primary: {
-                main: "#bf360c"
-            }
-        }
-    })
+ 
 
     return (
-        <ThemeProvider theme={theme}>
         <footer id="footer">
 
-            <div style={{
-            marginBottom: '20px',
-            }}>
-                <Link className="links" href="https://github.com/HeatMarie" target="_blank">
-                <GitHubIcon style={{ fontSize: 45 }} color="primary"></GitHubIcon>
+            <div>
+                <Link className="footer-links" href="https://github.com/HeatMarie" target="_blank">
+                <GitHubIcon style={{ fontSize: 40 }}></GitHubIcon>
 
                 </Link>
-                <Link className="links" href="https://www.linkedin.com/in/heather-bjoin-bb833761/" target="_blank">
-                <LinkedInIcon style={{ fontSize: 45 }} color="primary"></LinkedInIcon>
+                <Link className="footer-links" href="https://www.linkedin.com/in/heather-bjoin-bb833761/" target="_blank">
+                <LinkedInIcon style={{ fontSize: 40 }}></LinkedInIcon>
 
                 </Link>
-                <Link className="links" href="https://heatmarie.com/" target="_blank">
-                <BookOutlinedIcon style={{ fontSize: 45 }} color="primary"></BookOutlinedIcon>
+                <Link className="footer-links" href="https://heatmarie.com/" target="_blank">
+                <BookOutlinedIcon style={{ fontSize: 40 }}></BookOutlinedIcon>
 
                 </Link>
             </div>
             <div className="footer">
-            <h4 className="footer-h3">Where imagination becomes <span className="footer-span">reality</span></h4>
+            <p className="footer-h3">Where i<span className='magic'>mag</span>inat<span className="magic">i</span>on be<span className='magic'>c</span>omes</p>
+            <p className="footer-span">reality</p>
             <p className="footer-p"> { '\u00A9' } Heather Bjoin</p>
             </div>
         </footer>
-
-        </ThemeProvider>
-
     )
 }
 
